d94cafb650a8d1bffdc0af0f9ece5d941e606709
dev30k <develop_kk@outlook.com>
2023-05-12 10:05:20 +0300

    starting exercise 6

---
+++ exer6.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+   
+
+
+)
+func camelcase(s string) int32 {
+    // Write your code here
+    for index,char := range s{
+      fmt.Printf("%-2d   %d",index,char)
+    }
+    
+
+}
+
+func main(){
+	
+}
+
+
+
